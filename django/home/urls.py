27b5f34e8720200100f1a3dc9d9335ada96bd854
Javier <jbantequera@gmail.com>
2019-06-04 18:21:11 +0200

    Feed añadido, favicon arreglado

--- django/home/urls.py
+++ django/home/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.custom_login, name='custom_login'),
+    url(r'^$', views.custom_login),
+    url(r'^feed', views.show_feed)
 ]
--- django/home/views.py
+++ django/home/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 # Create your views here.
 def custom_login(request):
     return render(request, "home/login.html", {})
+
+def show_feed(request):
+    return render(request, "home/feed.html", {})
